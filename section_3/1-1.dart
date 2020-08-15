a28eabfe621b0641e05289ff81a09fda77e7f122
yuky_az <yuky.azuma@gmail.com>
2020-08-15 14:33:08 +0900

    Create 1-1.dart

---
+++ section_3/1-1.dart
@@ -0,0 +1,21 @@
+import 'package:flutter/material.dart';
+
+void main() => runApp(MyApp()); // 引数のWidgetが画面いっぱいに表示される
+
+// 最初に表示するWidgetのクラス
+class MyApp extends StatelessWidget {  // StatelessWidgetを継承
+  @override
+  Widget build(BuildContext context) {  //buildメソッドでUIを作成
+    return MaterialApp(  // マテリアルデザインのアプリ
+      title: "My Simple App",  // アプリのタイトル
+      home: Scaffold(  // マテリアルデザインの土台
+        appBar: AppBar(  // ヘッダーに表示するアプリケーションバー
+          title: Text("Live!人工知能"),  // タイトルを表示
+        ),
+        body: Center(  // 中央に配置
+          child: Text("Hello Widgets!"),  // 文字列を配置
+        ),
+      ),
+    );
+  }
+}
