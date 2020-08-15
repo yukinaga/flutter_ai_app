57764a43e9fe96e389b602b4a9fd4c384e0ae8d4
yuky_az <yuky.azuma@gmail.com>
2020-08-15 18:45:56 +0900

    Create 1-3_main.dart

---
+++ section_3/1-3_main.dart
@@ -0,0 +1,38 @@
+import 'package:flutter/material.dart';
+import 'package:english_words/english_words.dart';
+
+void main() => runApp(MyApp()); // 引数のWidgetが画面いっぱいに表示される
+
+// 状態を管理するクラスは、Stateクラスを継承
+class RandomWordsState extends State<RandomWords> {
+  @override
+  Widget build(BuildContext context) {
+    final wordPair = WordPair.random();
+    return Text(wordPair.asPascalCase);
+  }
+}
+
+// StatefulなWidgetのクラスは、StatefulWidgetを継承
+class RandomWords extends StatefulWidget {
+  @override
+  RandomWordsState createState() => RandomWordsState();
+}
+
+// 最初に表示するWidgetのクラス
+class MyApp extends StatelessWidget {  // StatelessWidgetを継承
+  @override
+  final wordPair = WordPair.random();  // ランダムな英単語のペア
+  Widget build(BuildContext context) {  //buildメソッドでUIを作成
+    return MaterialApp(  // マテリアルデザインのアプリ
+      title: "My Simple App",  // アプリのタイトル
+      home: Scaffold(  // マテリアルデザインの土台
+        appBar: AppBar(  // ヘッダーに表示するアプリケーションバー
+          title: Text("Live!人工知能"),  // タイトルを表示
+        ),
+        body: Center(  // 中央に配置
+          child: RandomWords(),
+        ),
+      ),
+    );
+  }
+}
