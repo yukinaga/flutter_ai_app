71acccf770fa57e809f1acaf9bc64ab4f5b6248c
yuky_az <yuky.azuma@gmail.com>
2020-08-16 22:42:34 +0900

    Create 3-2_main.dart

---
+++ section_3/3_interactive/3-2_main.dart
@@ -0,0 +1,58 @@
+/* 3-2. テキストの入力 */
+
+import 'package:flutter/material.dart';
+
+void main() => runApp(MyApp());
+
+class MyApp extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      title: "My Simple App",
+      home: Scaffold(
+        appBar: AppBar(
+          title: Text("Live!人工知能"),
+        ),
+        body: Center(
+          child: ChangeForm(),
+        ),
+      ),
+    );
+  }
+}
+
+class ChangeForm extends StatefulWidget {
+  @override
+  _ChangeFormState createState() => _ChangeFormState();
+}
+
+class _ChangeFormState extends State<ChangeForm> {
+  String _text = '';
+
+  void _handleText(String e) {
+    setState(() {  // 状態を保持する変数を変更する処理は、setState内に記述する
+      _text = e;
+    });
+  }
+
+  Widget build(BuildContext context) {
+    return Container(
+        child: Column(
+          mainAxisAlignment: MainAxisAlignment.center,
+          children: <Widget>[
+            Text(
+              "$_text",
+              style: TextStyle(
+                  color:Colors.blueAccent,
+                  fontSize: 30.0,
+              ),
+            ),
+            new TextField(  // テキストを表示
+              style: TextStyle(color: Colors.red),
+              onChanged: _handleText,
+            ),
+          ],
+        )
+    );
+  }
+}
