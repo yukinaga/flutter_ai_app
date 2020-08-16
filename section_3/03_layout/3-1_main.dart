ac4541e388d5822cbfd152c418c3860e01e801e4
yuky_az <yuky.azuma@gmail.com>
2020-08-16 11:41:09 +0900

    Create 3-1_main.dart

---
+++ section_3/03_layout/3-1_main.dart
@@ -0,0 +1,23 @@
+import 'package:flutter/material.dart';
+
+void main() => runApp(MyApp()); // 引数のWidgetが画面いっぱいに表示される
+
+class MyApp extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      title: "My Simple App",
+      home: Scaffold(
+        body: Center(
+          child: Container(
+            color: Colors.blue,
+            width: 300.0,
+            height: 300.0,
+            child: Text("Live!人工知能"),
+            alignment: Alignment.center,
+          ),
+        ),
+      ),
+    );
+  }
+}
