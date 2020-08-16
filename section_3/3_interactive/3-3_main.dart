52905b8c30dbe092ea1e9af89580d27826b597a3
yuky_az <yuky.azuma@gmail.com>
2020-08-16 22:51:10 +0900

    Create 3-3_main.dart

---
+++ section_3/3_interactive/3-3_main.dart
@@ -0,0 +1,58 @@
+/* 3-2. スイッチ */
+
+import 'package:flutter/material.dart';
+
+void main() => runApp(MyApp());
+
+class MyApp extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      title: "My Simple App",
+      home: Scaffold(
+        appBar: AppBar(
+          title: Text("Live!人工知能"),
+        ),
+        body: Center(
+          child: ChangeForm(),
+        ),
+      ),
+    );
+  }
+}
+
+class ChangeForm extends StatefulWidget {
+  @override
+  _ChangeFormState createState() => _ChangeFormState();
+}
+
+class _ChangeFormState extends State<ChangeForm> {
+  bool _active = false;
+
+  void _changeSwitch(bool e) {
+    setState(() {  // 状態を保持する変数を変更する処理は、setState内に記述する
+      _active = e;
+    });
+  }
+
+  Widget build(BuildContext context) {
+    return Container(
+        child: Column(
+          mainAxisAlignment: MainAxisAlignment.center,
+          children: <Widget>[
+            Center(
+              child: new Icon(
+                Icons.thumb_up,
+                color: _active ? Colors.orange : Colors.grey,
+                size: 100.0,
+              ),
+            ),
+            new Switch(  // スイッチ
+              value: _active,
+              onChanged: _changeSwitch,
+            )
+          ],
+        )
+    );
+  }
+}
