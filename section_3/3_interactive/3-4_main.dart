89ff93cd5a4268c6187d672254579fbe38888691
yuky_az <yuky.azuma@gmail.com>
2020-08-16 23:12:18 +0900

    Create 3-4_main.dart

---
+++ section_3/3_interactive/3-4_main.dart
@@ -0,0 +1,58 @@
+/* 3-4. スライダー */
+
+import 'package:flutter/material.dart';
+
+void main() => runApp(MyApp());
+
+class MyApp extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      title: "My Simple App",
+      home: Scaffold(
+        appBar: AppBar(
+          title: Text("Live!人工知能"),
+        ),
+        body: Center(
+          child: ChangeForm(),
+        ),
+      ),
+    );
+  }
+}
+
+class ChangeForm extends StatefulWidget {
+  @override
+  _ChangeFormState createState() => _ChangeFormState();
+}
+
+class _ChangeFormState extends State<ChangeForm> {
+  double _value = 0.0;
+
+  void _changeSlider(double e) {
+    setState(() {  // 状態を保持する変数を変更する処理は、setState内に記述する
+      _value = e;
+    });
+  }
+
+  Widget build(BuildContext context) {
+    return Container(
+        child: Column(
+          mainAxisAlignment: MainAxisAlignment.center,
+          children: <Widget>[
+            Center(child:Text("値：${_value}")),
+            Slider(  // スライダー
+              label: '${_value}',
+              min: 0,
+              max: 10,
+              value: _value,
+              activeColor: Colors.orange,
+              inactiveColor: Colors.blueAccent,
+              divisions: 10,
+              onChanged: _changeSlider,
+            )
+          ],
+        )
+    );
+  }
+}
