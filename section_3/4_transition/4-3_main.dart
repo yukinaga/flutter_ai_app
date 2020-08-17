abd5e473e0c72c024f9ae702755e056ff312ceb2
yuky_az <yuky.azuma@gmail.com>
2020-08-17 16:01:50 +0900

    Create 4-3_main.dart

---
+++ section_3/4_transition/4-3_main.dart
@@ -0,0 +1,50 @@
+/* 4-3. Drawer */
+
+import 'package:flutter/material.dart';
+
+void main() => runApp(MyApp());
+
+class MyApp extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      home: MainPage(),
+    );
+  }
+}
+
+class MainPage extends StatefulWidget {
+  @override
+  _MainPageState createState() => _MainPageState();
+}
+
+class _MainPageState extends State<MainPage> {
+
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        title: const Text("Live!人工知能"),
+      ),
+      drawer: Drawer( // Drawerの配置
+        child: ListView(
+          children: <Widget>[
+            DrawerHeader(
+              child: Text("Drawer Header"),
+              decoration: BoxDecoration(  // Boxによる装飾
+                color: Colors.blue,
+              ),
+            ),
+            ListTile(
+              title: Text("Item 1"),
+              trailing: Icon(Icons.arrow_forward),
+            ),
+            ListTile(
+              title: Text("Item 2"),
+              trailing: Icon(Icons.arrow_forward),
+            ),
+          ],
+        ),
+      ),
+    );
+  }
+}
