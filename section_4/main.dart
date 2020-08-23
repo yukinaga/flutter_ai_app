ddab528127b6aa1deab5305a21f7fe8d473f8c13
yuky_az <yuky.azuma@gmail.com>
2020-08-23 21:06:01 +0900

    Update main.dart

--- section_4/main.dart
+++ section_4/main.dart
@@ -12,7 +12,7 @@ class MyApp extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
-      title: 'Baby Names',
+      title: "Dog Name Voting",
       home: MyHomePage(),
     );
   }
