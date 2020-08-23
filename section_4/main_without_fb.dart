f6cbe0d96cdeb1da5f9f25d899eecef04754f0e3
yuky_az <yuky.azuma@gmail.com>
2020-08-23 22:13:49 +0900

    Create main_without_fb.dart

---
+++ section_4/main_without_fb.dart
@@ -0,0 +1,73 @@
+import 'package:flutter/material.dart';
+
+final dummyData = [
+  {"name": "Pochi", "votes": 1},
+  {"name": "Taro", "votes": 4},
+  {"name": "Jiro", "votes": 2},
+  {"name": "Shiro", "votes": 0},
+  {"name": "Hachi", "votes": 3},
+];
+
+void main()  async{
+  runApp(MyApp());
+}
+
+class MyApp extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      title: "Dog Name Voting",
+      home: MyHomePage(),
+    );
+  }
+}
+
+class MyHomePage extends StatefulWidget {
+  @override
+  _MyHomePageState createState() {
+    return _MyHomePageState();
+  }
+}
+
+class _MyHomePageState extends State<MyHomePage> {
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(title: Text("Dog Name Voting")),
+      body: _buildBody(),
+    );
+  }
+
+  Widget _buildBody() {
+    return _buildList(dummyData);
+  }
+
+  Widget _buildList(List<Map<String, dynamic>> dataList) {
+    return ListView.builder(
+        padding: const EdgeInsets.all(18.0),
+        itemCount: dataList.length,
+        itemBuilder: (context, i) {
+          return _buildListItem(dataList[i]);
+        }
+    );
+  }
+
+  Widget _buildListItem(Map<String, dynamic> data) {
+    return Padding(
+      padding: const EdgeInsets.symmetric(horizontal: 18.0, vertical:9.0),
+      child: Container(
+        decoration: BoxDecoration(
+          border: Border.all(color: Colors.grey),
+          borderRadius: BorderRadius.circular(8.0),
+        ),
+        child: ListTile(
+          title: Text(data["name"]),
+          trailing: Text(data["votes"].toString()),
+          onTap: () {
+            setState(() { data["votes"] += 1; });
+            },
+        ),
+      ),
+    );
+  }
+}
